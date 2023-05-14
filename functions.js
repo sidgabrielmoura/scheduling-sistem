2d25637d50791bd879860da3ab6d1cd4a7ef4315
sidgabrielmoura <sidgabrielmoura40@gmail.com>
2023-05-14 19:58:03 -0300

    create a repository in github

---
+++ functions.js
@@ -0,0 +1,27 @@
+const closeModal = document.querySelector("#close-modal")
+const openModal = document.querySelector("#plus")
+const confirmName = document.querySelector("#confirm-the-name")
+
+let toggleModal = () => {
+    modal.classList.toggle("hide")
+    fade.classList.toggle("hide")
+}
+[openModal, closeModal, fade, confirmName].forEach((e) => {
+    e.addEventListener('click', () => toggleModal())
+})
+
+function confirma() {
+
+    let valor = newPerson.value
+
+    if(valor != ""){
+        let novaPessoa = document.createElement("h3")
+        let texto  = document.createTextNode(valor)
+        novaPessoa.appendChild(texto)
+        novaPessoa.classList.add("people-window-views")
+
+        htmlMain.appendChild(novaPessoa)
+    }else{
+        alert("talvez você tenha esquecido o nome! confere aí.")
+    }  
+}
---
+++ variaveis.js
@@ -0,0 +1,10 @@
+let search = document.querySelector("#search-people")
+let people = document.querySelector(".people-window-views")
+let htmlMain = document.querySelector("main")
+
+//MODAL
+
+const modal = document.querySelector("#modal")
+const fade = document.querySelector("#fade")
+
+var newPerson = document.querySelector("#name-of-the-new-person")
